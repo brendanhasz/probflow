ecf89fcc30ab0d5ebe79f9d227de033cfe3733ec
Brendan Hasz <winsto99@gmail.com>
2018-12-20 17:45:34 -0600

    Add skeleton of BaseModel

--- bk/models.py
+++ bk/models.py
@@ -1,57 +1,198 @@
 """Pre-built models.
 """
 
-#imports...
+from abc import ABC, abstractmethod
 
-class _BaseModel():
+class _BaseModel(ABC):
     """Abstract model class (just used as implementation base)
 
-
-    More info...
-
+    TODO: More info...
 
     """
 
-    def __init__(self, ):
+
+    def __init__(self, *args, **kwargs):
         """Construct model.
 
-        Args:
-          ...
+        TODO: docs
         """
+        self.args = args
+        self.kwargs = kwargs
         self.built_model = None
 
+
+    @abstractmethod
     def build(self, data):
         """Build model.
         
-        ...
+        Inheriting class must define this method by building the 
+        model for that class.  Should set `self.built_model` to a
+        TensorFlow Probability distribution, using the inputs to 
+        the constructor which are stored in `self.args` and 
+        `self.kwargs`.
         """
-        # TODO: build child models
-        # TODO: Construct this from built child models
+        pass
+
 
     def fit(self, x, y, batch_size=128, epochs=100, 
             optimizer='adam', learning_rate=None, metrics=None):
         """Fit model.
 
-        Docs...
+        TODO: Docs...
+
         """
         # TODO: build the model
         # TODO: set up the data for fitting
         # TODO: fit the model
 
-    # TODO:
-    """
-    - predict(x)
-    - residuals(x, y)
-    - plot_residuals(x, y)
-    - metrics(x, y, metric_list)
-    - posterior(params=None)
-    - predictive_distribution(x)
-    - predictive_prc(x, y)
-    - pred_dist_covered(x, y, prc)
-    - pred_dist_coverage(x, y, prc)
-    - calibration_curve(x, y)
-    - coverage_by(x, y, x_by, prc)
-    """
+
+    def predict(self, x):
+        """Predict dependent variable for samples in x.
+
+        TODO: Docs...
+
+        """
+        #TODO
+
+
+    def residuals(self, x, y):
+        """Compute residuals between true and predicted values. 
+
+        TODO: Docs...
+
+        """
+        #TODO
+
+
+    def plot_residuals(self, x, y):
+        """Plot the residual distribution.
+
+        TODO: Docs...
+
+        """
+        #TODO
+
+
+    def metrics(self, x, y, metric_list):
+        """Compute prediction metrics.
+
+        # Arguments
+            x: validation independent variable samples.
+            y: validation dependent variable samples.
+            metric_list: list of metrics to evaluate.
+
+        TODO: Docs...
+
+        """
+        #TODO
+
+        
+    def posterior(self, params=None):
+        """Predict dependent variable for samples in x.
+
+        TODO: Docs...
+
+        """
+        #TODO
+
+        
+    def predictive_distribution(self, x):
+        """Predict dependent variable for samples in x.
+
+        TODO: Docs...
+
+        """
+        #TODO
+
+        
+    def predictive_prc(self, x, y):
+        """Predict dependent variable for samples in x.
+
+        TODO: Docs...
+
+        """
+        #TODO
+
+        
+    def pred_dist_covered(self, x, y, prc):
+        """Predict dependent variable for samples in x.
+
+        TODO: Docs...
+
+        """
+        #TODO
+
+        
+    def pred_dist_coverage(self, x, y, prc):
+        """Predict dependent variable for samples in x.
+
+        TODO: Docs...
+
+        """
+        #TODO
+
+        
+    def calibration_curve(self, x, y):
+        """Predict dependent variable for samples in x.
+
+        TODO: Docs...
+
+        """
+        #TODO
+
+        
+    def coverage_by(self, x, y, x_by, prc):
+        """Predict dependent variable for samples in x.
+
+        TODO: Docs...
+
+        """
+        #TODO
 
 
 class LinearRegression(_BaseModel):
+    #TODO
+
+
+class LogisticRegression(_BaseModel):
+    #TODO
+
+
+class DenseRegression(_BaseModel):
+    #TODO
+
+
+class DenseClassifier(_BaseModel):
+    #TODO
+
+
+class Conv1dRegression(_BaseModel):
+    #TODO
+
+
+class Conv1dClassifier(_BaseModel):
+    #TODO
+
+
+class Conv2dRegression(_BaseModel):
+    #TODO
+
+
+class Conv2dClassifier(_BaseModel):
+    #TODO
+
+
+class DenseAutoencoder(_BaseModel):
+    #TODO
+
+
+class Conv1dAutoencoder(_BaseModel):
+    #TODO
+
+
+class Conv2dAutoencoder(_BaseModel):
+    #TODO
+
+#TODO: Neural matrix factorization
+
+#TODO: BayesianCorrelation
