3b7cb2f1a646aac0909135387a12b5564f75e6a1
Brendan Hasz <winsto99@gmail.com>
2019-09-10 23:29:11 -0500

    Aight we DO need tf to build the docs

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import find_packages, setup
 
 setup(name='probflow',
-      version='2.0.0',
-      description='A Python package for building Bayesian models with TensorFlow or PyTorch',
-      url='https://github.com/brendanhasz/probflow',
-      author='Brendan Hasz',
-      author_email='winsto99@gmail.com',
-      license='MIT',
-      packages=find_packages(where="src"),
-      package_dir={"": "src"},
-      zip_safe=False)
+    version='2.0.0',
+    description='A Python package for building Bayesian models with TensorFlow or PyTorch',
+    url='https://github.com/brendanhasz/probflow',
+    author='Brendan Hasz',
+    author_email='winsto99@gmail.com',
+    license='MIT',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    zip_safe=False,
+    extras_require={
+        'tests': [
+            'tensorflow == 2.0.0b1',
+            'tfp-nightly >= 0.9.0',
+            'torch >= 1.2.0',
+            'pytest >= 5.1.2',
+            'pylint >= 2.3.1',
+            ],
+        'docs': [
+            'tensorflow == 2.0.0b1',
+            'tfp-nightly >= 0.9.0',
+            ]
+    }
+)
