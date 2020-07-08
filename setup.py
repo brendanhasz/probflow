3f62838894c8ac1bbf784af4b19e2a80bb61eb8a
Brendan Hasz <winsto99@gmail.com>
2020-07-07 20:19:40 -0500

    Update TF version dependency and release v 2.0.0a3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as f:
 
 setup(
     name='probflow',
-    version='2.0.0a2', #NOTE: also in __init__.py
+    version='2.0.0a3', #NOTE: also in __init__.py
     author='Brendan Hasz',
     author_email='winsto99@gmail.com',
     description='A Python package for building Bayesian models with TensorFlow or PyTorch',
@@ -24,16 +24,16 @@ setup(
     zip_safe=False,
     extras_require={
         'tests': [
-            'tensorflow == 2.0.0',
-            'tensorflow-probability == 0.8.0',
+            'tensorflow == 2.2.0',
+            'tensorflow-probability == 0.10.0',
             'torch >= 1.2.0',
             'pytest >= 5.1.2',
             'pytest-cov >= 2.7.1',
             'pylint >= 2.3.1',
             ],
         'docs': [
-            'tensorflow == 2.0.0',
-            'tensorflow-probability == 0.8.0',
+            'tensorflow == 2.2.0',
+            'tensorflow-probability == 0.10.0',
             'sphinx-tabs'
             ]
     }
