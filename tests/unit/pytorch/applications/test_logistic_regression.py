80a6456a24565c0854264f8c47b40a5676a2914f
Brendan Hasz <winsto99@gmail.com>
2020-12-26 03:05:09 -0600

    Add tests for pytorch applications

--- tests/unit/pytorch/applications/test_logistic_regression.py
+++ tests/unit/pytorch/applications/test_logistic_regression.py
@@ -40,3 +40,43 @@ def test_MultinomialLogisticRegression():
 
     # Predictive functions
     model.predict(x)
+
+
+def test_LogisticRegression_multiMC_eager():
+    """Tests LinearRegression w/ n_mc>1 in eager mode"""
+
+    # Data
+    x = np.random.randn(100, 5).astype("float32")
+    w = np.random.randn(5, 3).astype("float32")
+    b = np.random.randn(1, 3).astype("float32")
+    y = x @ w + b
+    y = np.argmax(y, axis=1).astype("int32")
+
+    # Create the model
+    model = LogisticRegression(d=5, k=3)
+
+    # Fit the model
+    model.fit(x, y, batch_size=50, epochs=2, n_mc=4, eager=True)
+
+    # Predictive functions
+    model.predict(x)
+
+
+def test_LogisticRegression_multiMC_noneager():
+    """Tests LinearRegression w/ n_mc>1 in non-eager mode"""
+
+    # Data
+    x = np.random.randn(100, 5).astype("float32")
+    w = np.random.randn(5, 3).astype("float32")
+    b = np.random.randn(1, 3).astype("float32")
+    y = x @ w + b
+    y = np.argmax(y, axis=1).astype("int32")
+
+    # Create the model
+    model = LogisticRegression(d=5, k=3)
+
+    # Fit the model
+    model.fit(x, y, batch_size=50, epochs=2, n_mc=4, eager=False)
+
+    # Predictive functions
+    model.predict(x)
