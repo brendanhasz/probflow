126704acda49d26cf69b8c0f61d03db79338e077
Brendan Hasz <winsto99@gmail.com>
2026-09-01 13:00:26 -0700

    fix logistic regression stats test

--- tests/stats/test_LogisticRegression.py
+++ tests/stats/test_LogisticRegression.py
@@ -4,30 +4,34 @@ import numpy as np
 
 import probflow as pf
 
+N_DATAPOINTS: int = 1000
+N_EPOCHS: int = 1000
+BATCH_SIZE: int = 100
+
 
 def test_logistic_regression():
     """Test that a logistic regression recovers the true parameters."""
 
-    # NOTE: this is failing for some reason, I don't know why
-
-    # Set random seed
-    # np.random.seed(1234)
-    # tf.random.set_seed(1234)
+    # Set random seed for reproducibility
+    np.random.seed(1234)
 
     # Generate data
-    N = 1000
-    D = 1
-    x = np.random.randn(N, D).astype("float32")
-    x_val = np.random.randn(N, D).astype("float32")
+    D = 3
+    x = np.random.randn(N_DATAPOINTS, D).astype("float32")
+    x_val = np.random.randn(N_DATAPOINTS, D).astype("float32")
     w = np.random.randn(D, 1)
     b = np.random.randn()
-    noise = 0.1 * np.random.randn(N, 1)
-    y = (1.0 / (1.0 + np.exp(-x @ w + b + noise)) > 0.5).astype("float32")
-    y_val = (1.0 / (1.0 + np.exp(-x @ w + b + noise)) > 0.5).astype("float32")
+    # Sample from: y ~ Bernoulli(sigmoid(x @ w + b))
+    p = 1.0 / (1.0 + np.exp(-(x @ w + b)))
+    y = (np.random.uniform(size=(N_DATAPOINTS, 1)) < p).astype("float32")
+    p_val = 1.0 / (1.0 + np.exp(-(x_val @ w + b)))
+    y_val = (np.random.uniform(size=(N_DATAPOINTS, 1)) < p_val).astype(
+        "float32"
+    )
 
     # Create and fit model
     model = pf.LogisticRegression(D)
-    model.fit(x, y, batch_size=100, epochs=1000, lr=1e-3)
+    model.fit(x, y, batch_size=BATCH_SIZE, epochs=N_EPOCHS, lr=1e-3)
 
     # Compute and check confidence intervals on the weights
     lb, ub = model.posterior_ci("weights")
