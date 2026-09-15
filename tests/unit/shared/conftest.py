a06b755874b692865f946f882e7db6a181a212f7
Brendan Hasz <winsto99@gmail.com>
2026-09-15 14:38:21 -0700

    update checks workflow w/ new shared tests

--- tests/unit/shared/conftest.py
+++ tests/unit/shared/conftest.py
@@ -1,7 +1,8 @@
+"""Fixtures for shared-backend unit tests."""
+
 import probflow as pf
 
 
 def pytest_runtest_setup(item):
-    """Provide pytest runtest setup."""
-    # pf.set_backend(pf.ProbflowBackend.TENSORFLOW)
+    """Provide pytest runtest setup for shared tests."""
     pf.set_datatype(None)
