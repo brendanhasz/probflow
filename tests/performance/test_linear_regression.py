70148806e1e1ed16503b08947c0df1c7fd07dd09
Brendan Hasz <winsto99@gmail.com>
2026-09-16 23:25:11 -0700

    update benchmarking script to only do one backend at a time

--- tests/performance/test_linear_regression.py
+++ tests/performance/test_linear_regression.py
@@ -10,49 +10,42 @@ EPOCHS = 100
 BATCH_SIZE = 1024
 ns = [1024, 8192, 65536]
 ds = [1, 2, 10, 100]
-backends = [
-    pf.ProbflowBackend.PYTORCH,
-    pf.ProbflowBackend.TENSORFLOW,
-    pf.ProbflowBackend.JAX,
-]
 eagers = [True, False]
 
 
 def get_data(N, D, dtype="float32"):
     """Provide get data."""
-    x = np.random.randn(N, D).astype(dtype)
-    w = np.random.randn(D, 1).astype(dtype)
-    y = x @ w + 0.1 * np.random.randn(D, 1).astype(dtype)
+    rng = np.random.default_rng(seed=1234)
+    x = rng.standard_normal((N, D)).astype(dtype)
+    w = rng.standard_normal((D, 1)).astype(dtype)
+    y = x @ w + 0.1 * rng.standard_normal((N, 1)).astype(dtype)
     return x, y
 
 
 def benchmark_linear_regression():
     """Test linear regression times."""
-    times = []
-    cached_data = {}
-    for N, D, backend, eager in product(ns, ds, backends, eagers):
-        pf.set_backend(backend)
+    data = []
+    backend = pf.get_backend()
+    for N, D, eager in product(ns, ds, eagers):
         model = pf.LinearRegression(D)
-        if (N, D) not in cached_data:
-            cached_data[(N, D)] = get_data(N, D)
-        x, y = cached_data[(N, D)]
+        x, y = get_data(N, D)
         t0 = time.time()
         model.fit(x, y, epochs=EPOCHS, eager=eager)
         t1 = time.time()
-        times.append(
+        data.append(
             {
                 "N": N,
                 "D": D,
-                "backend": backend,
+                "backend": backend.value,
                 "eager": eager,
                 "seconds": t1 - t0,
             }
         )
 
-    df = pd.DataFrame.from_records(times)
+    df = pd.DataFrame.from_records(data)
 
     # Save the results to a CSV file
-    df.to_csv("benchmark_linear_regression.csv", index=False)
+    df.to_csv(f"benchmark_linear_regression_{backend.value}.csv", index=False)
 
 
 if __name__ == "__main__":
